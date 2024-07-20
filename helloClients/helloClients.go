baafed80bf937f3be331c402c28055982cc5c997
Comprehensher <boltankove@gmail.com>
2024-07-20 13:02:27 +0300

    Error handling, one module raises error, another handles it

--- helloClients/helloClients.go
+++ helloClients/helloClients.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"eu.corp/productlist"
 	"rsc.io/quote"
 )
 
 func main() {
+	log.SetPrefix("From shop: ")
+	log.SetFlags(0)
+
 	fmt.Println(quote.Go())
-	fmt.Printf("Milk has price %v", productlist.GetProductPrice("Milk"))
+	price, error := productlist.GetProductPrice("Milk222")
+	if error != nil {
+		log.Fatal(error)
+	}
+	fmt.Printf("Milk has price %v", price)
 }
--- productList/productList.go
+++ productList/productList.go
@@ -1,11 +1,21 @@
 package productlist
 
-func GetProductPrice(productName string) int {
+import (
+	"errors"
+)
+
+func GetProductPrice(productName string) (int, error) {
 	productMap := make(map[string]int)
 	productMap["Bread"] = 25
 	productMap["Milk"] = 85
 	productMap["Sugar"] = 39
 
-	return productMap[productName]
+	if productName == "" {
+		return 0, errors.New("Product name is empty!")
+	} else if productName != "Bread" && productName != "Milk" && productName != "Sugar" {
+		return 0, errors.New("Product name does not exist!")
+	}
+
+	return productMap[productName], nil
 
 }
