1cd9cc560433b2c3a846e4d28c51315a113c4995
Comprehensher <boltankove@gmail.com>
2024-07-26 16:47:04 +0300

    Close channel in sender when channel does not need. Check in receiver whether channel is open. It helps when situation when we don't know how many values will be sent by goroutines.

--- concurrency/main.go
+++ concurrency/main.go
@@ -9,5 +9,18 @@ func main() {
 	fmt.Println("main function started")
 	CalcStoreTotal(Products)
 	// time.Sleep(time.Second * 5)
-	fmt.Println("main function complete")
+	fmt.Println("main function complete calculation")
+
+	dispatchChannel := make(chan DispatchNotification, 100)
+	go DispatchOrders(dispatchChannel)
+	for {
+		if details, open := <-dispatchChannel; open {
+			fmt.Println("Dispatch to", details.Customer, ":",
+				details.Quantity,
+				"x", details.Product.Name)
+		} else {
+			fmt.Println("Channel has been closed")
+			break
+		}
+	}
 }
