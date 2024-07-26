453004d0c6b65cc1d2a4a26884440c9b733ab5d4
Comprehensher <boltankove@gmail.com>
2024-07-26 15:41:52 +0300

    Example how can receivers and acceptors of channels can be blocked for some time Example only for studying and analyze

--- concurrency/operations.go
+++ concurrency/operations.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func CalcStoreTotal(data ProductData) {
 	var storeTotal float64
@@ -8,17 +11,24 @@ func CalcStoreTotal(data ProductData) {
 	for category, group := range data {
 		go group.TotalPrice(category, channel)
 	}
+	time.Sleep(time.Second * 5)
+	fmt.Println("-- Starting to receive from channel")
 	for i := 0; i < len(data); i++ {
-		storeTotal += <-channel
+		fmt.Println("-- channel read pending")
+		value := <-channel
+		fmt.Println("-- channel read complete", value)
+		storeTotal += value
+		time.Sleep(time.Second)
 	}
 	fmt.Println("Total:", ToCurrency(storeTotal))
 }
 func (group ProductGroup) TotalPrice(category string, resultChannel chan float64) {
 	var total float64
 	for _, p := range group {
-		fmt.Println(category, "product:", p.Name)
 		total += p.Price
 	}
 	fmt.Println(category, "subtotal:", ToCurrency(total))
+	fmt.Println(category, "channel sending", ToCurrency(total))
 	resultChannel <- total
+	fmt.Println(category, "channel send complete")
 }
