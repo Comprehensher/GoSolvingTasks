81f6ec05d425a909d1fa4b5e9262232566751f60
Comprehensher <boltankove@gmail.com>
2024-07-26 13:58:38 +0300

    Using channel for getting results from goroutines Receiving channel result in main, force routine main wait to get channel result

--- concurrency/main.go
+++ concurrency/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"time"
+	// "time"
 )
 
 func main() {
 	fmt.Println("main function started")
 	CalcStoreTotal(Products)
-	time.Sleep(time.Second * 5)
+	// time.Sleep(time.Second * 5)
 	fmt.Println("main function complete")
 }
