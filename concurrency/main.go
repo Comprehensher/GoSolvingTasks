c627456cb469c6357091384ac7e8c86c11ab253b
Comprehensher <boltankove@gmail.com>
2024-07-29 11:09:54 +0300

    Receive values from two different channels "Select" allows receiver to get value from any channel without lock definite channel

--- concurrency/main.go
+++ concurrency/main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"fmt"
 	"time"
-	// "time"
 )
 
-// func receiveDispatches(channel <-chan DispatchNotification) {
-// 	for details := range channel {
-// 		fmt.Println("Dispatch to", details.Customer, ":", details.Quantity, "x", details.Product.Name)
-// 	}
-// 	fmt.Println("Channel has been closed")
-// }
+func enumerateProducts(channel chan<- *Product) {
+	for _, p := range ProductList[:3] {
+		channel <- p
+		time.Sleep(time.Millisecond * 800)
+	}
+	close(channel)
+}
 
 func main() {
 	fmt.Println("main function started")
@@ -20,21 +20,33 @@ func main() {
 	fmt.Println("main function complete calculation")
 
 	dispatchChannel := make(chan DispatchNotification, 100)
-	// var sendOnlyChannel chan<- DispatchNotification = dispatchChannel
-	// var receiveOnlyChannel <-chan DispatchNotification = dispatchChannel
-
 	go DispatchOrders(chan<- DispatchNotification(dispatchChannel))
-	// receiveDispatches((<-chan DispatchNotification)(dispatchChannel))
+	productChannel := make(chan *Product)
+	go enumerateProducts(productChannel)
+	openChannels := 2
+
 	for {
 		select {
 		case details, ok := <-dispatchChannel:
 			if ok {
 				fmt.Println("Dispatch to", details.Customer, ":", details.Quantity, "x", details.Product.Name)
 			} else {
-				fmt.Println("Channel has been closed")
-				goto alldone
+				fmt.Println("Dispatch channel has been closed")
+				dispatchChannel = nil
+				openChannels--
+			}
+		case product, ok := <-productChannel:
+			if ok {
+				fmt.Println("Product:", product.Name)
+			} else {
+				fmt.Println("Product channel has been closed")
+				productChannel = nil
+				openChannels--
 			}
 		default:
+			if openChannels == 0 {
+				goto alldone
+			}
 			fmt.Println("-- No message ready to be received")
 			time.Sleep(time.Millisecond * 500)
 		}
