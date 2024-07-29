3169937f6c93280531b626ddd38eea0c30fe89d9
Comprehensher <boltankove@gmail.com>
2024-07-29 10:05:30 +0300

    Use select when receiving channel and no lock on receiver. Imitate situation, when values from sender comes with some delay, and you don’t want to lock receiver, you wish to execute code in default section in select.

--- concurrency/main.go
+++ concurrency/main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
+	"time"
 	// "time"
 )
 
-func receiveDispatches(channel <-chan DispatchNotification) {
-	for details := range channel {
-		fmt.Println("Dispatch to", details.Customer, ":", details.Quantity, "x", details.Product.Name)
-	}
-	fmt.Println("Channel has been closed")
-}
+// func receiveDispatches(channel <-chan DispatchNotification) {
+// 	for details := range channel {
+// 		fmt.Println("Dispatch to", details.Customer, ":", details.Quantity, "x", details.Product.Name)
+// 	}
+// 	fmt.Println("Channel has been closed")
+// }
 
 func main() {
 	fmt.Println("main function started")
@@ -19,9 +20,25 @@ func main() {
 	fmt.Println("main function complete calculation")
 
 	dispatchChannel := make(chan DispatchNotification, 100)
-	var sendOnlyChannel chan<- DispatchNotification = dispatchChannel
-	var receiveOnlyChannel <-chan DispatchNotification = dispatchChannel
+	// var sendOnlyChannel chan<- DispatchNotification = dispatchChannel
+	// var receiveOnlyChannel <-chan DispatchNotification = dispatchChannel
 
-	go DispatchOrders(sendOnlyChannel)
-	receiveDispatches(receiveOnlyChannel)
+	go DispatchOrders(chan<- DispatchNotification(dispatchChannel))
+	// receiveDispatches((<-chan DispatchNotification)(dispatchChannel))
+	for {
+		select {
+		case details, ok := <-dispatchChannel:
+			if ok {
+				fmt.Println("Dispatch to", details.Customer, ":", details.Quantity, "x", details.Product.Name)
+			} else {
+				fmt.Println("Channel has been closed")
+				goto alldone
+			}
+		default:
+			fmt.Println("-- No message ready to be received")
+			time.Sleep(time.Millisecond * 500)
+		}
+	}
+alldone:
+	fmt.Println("All values received")
 }
