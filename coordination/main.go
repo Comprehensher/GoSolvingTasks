ee2f47e7295b0848be706ffd8c8640f6299f1973
Comprehensher <boltankove@gmail.com>
2024-07-30 11:07:32 +0300

    RWMutex for read and writing values to map

--- coordination/main.go
+++ coordination/main.go
@@ -1,33 +1,44 @@
 package main
 
 import (
+	"math"
+	"math/rand"
 	"sync"
 	"time"
 )
 
 var waitGroup = sync.WaitGroup{}
-var mutex = sync.Mutex{}
+var rwmutex = sync.RWMutex{}
 
-func doSum(count int, val *int) {
-	time.Sleep(time.Second * 2)
-	mutex.Lock()
-	for i := 0; i < count; i++ {
+var squares = map[int]int{}
 
-		*val++
-
+func calculateSquares(max, iterations int) {
+	for i := 0; i < iterations; i++ {
+		val := rand.Intn(max)
+		rwmutex.RLock()
+		square, ok := squares[val]
+		rwmutex.RUnlock()
+		if ok {
+			Printfln("Cached value: %v = %v", val, square)
+		} else {
+			rwmutex.Lock()
+			if _, ok := squares[val]; !ok {
+				squares[val] = int(math.Pow(float64(val), 2))
+				Printfln("Added value: %v = %v", val, squares[val])
+			}
+			rwmutex.Unlock()
+		}
 	}
-	mutex.Unlock()
 	waitGroup.Done()
 }
-
 func main() {
-	counter := 0
-
+	rand.Seed(time.Now().UnixNano())
+	//counter := 0
 	numRoutines := 3
 	waitGroup.Add(numRoutines)
 	for i := 0; i < numRoutines; i++ {
-		go doSum(5000, &counter)
+		go calculateSquares(10, 5)
 	}
 	waitGroup.Wait()
-	Printfln("Total: %v", counter)
+	Printfln("Cached values: %v", len(squares))
 }
