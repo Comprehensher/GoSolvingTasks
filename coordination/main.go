71bd6009966292abc6f2351672eac89d4cc545f6
Comprehensher <boltankove@gmail.com>
2024-07-30 09:56:52 +0300

    Method main should wait gorouitne. Use WaitGroup

--- coordination/main.go
+++ coordination/main.go
@@ -1,13 +1,23 @@
 package main
 
+import (
+	"sync"
+)
+
+var waitGroup = sync.WaitGroup{}
+
 func doSum(count int, val *int) {
 	for i := 0; i < count; i++ {
 		*val++
 	}
+	waitGroup.Done()
 }
 
 func main() {
 	counter := 0
+
+	waitGroup.Add(1)
 	doSum(5000, &counter)
+	waitGroup.Wait()
 	Printfln("Total: %v", counter)
 }
