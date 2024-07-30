ae1c189d394e2ddd6ae7d156b2b13956917f7515
Comprehensher <boltankove@gmail.com>
2024-07-30 14:25:47 +0300

    Coordinate goroutines. Use sync.NewCond. One gourutine should write to map, another two should read from map after the first goroutine fills whole map.

--- coordination/main.go
+++ coordination/main.go
@@ -10,35 +10,52 @@ import (
 var waitGroup = sync.WaitGroup{}
 var rwmutex = sync.RWMutex{}
 
+var readyCond = sync.NewCond(rwmutex.RLocker())
+
+// var readyCond = sync.NewCond(&rwmutex)
+
 var squares = map[int]int{}
 
-func calculateSquares(max, iterations int) {
-	for i := 0; i < iterations; i++ {
-		val := rand.Intn(max)
-		rwmutex.RLock()
-		square, ok := squares[val]
-		rwmutex.RUnlock()
-		if ok {
-			Printfln("Cached value: %v = %v", val, square)
-		} else {
-			rwmutex.Lock()
-			if _, ok := squares[val]; !ok {
-				squares[val] = int(math.Pow(float64(val), 2))
-				Printfln("Added value: %v = %v", val, squares[val])
-			}
-			rwmutex.Unlock()
-		}
+func generateSquares(max int) {
+	rwmutex.Lock()
+	Printfln("Generating data...")
+	for val := 0; val < max; val++ {
+		squares[val] = int(math.Pow(float64(val), 2))
 	}
+	rwmutex.Unlock()
+	Printfln("Broadcasting condition")
+	readyCond.Broadcast()
 	waitGroup.Done()
 }
+
+func readSquares(id, max, iterations int) {
+	readyCond.L.Lock()
+	Printfln("Goroutine %v has got lock", id)
+	for len(squares) == 0 {
+		Printfln("Goroutine %v starts to wait...", id)
+		readyCond.Wait()
+		Printfln("Goroutine %v finish to wait...", id)
+	}
+	for i := 0; i < iterations; i++ {
+		key := rand.Intn(max)
+		Printfln("#%v Read value: %v = %v", id, key, squares[key])
+		time.Sleep(time.Millisecond * 100)
+	}
+	readyCond.L.Unlock()
+	Printfln("Goroutine %v released lock", id)
+	waitGroup.Done()
+}
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	//counter := 0
-	numRoutines := 3
+	numRoutines := 2
 	waitGroup.Add(numRoutines)
 	for i := 0; i < numRoutines; i++ {
-		go calculateSquares(10, 5)
+		go readSquares(i, 10, 5)
 	}
+	time.Sleep(time.Millisecond * 200)
+	waitGroup.Add(1)
+	go generateSquares(10)
 	waitGroup.Wait()
 	Printfln("Cached values: %v", len(squares))
 }
