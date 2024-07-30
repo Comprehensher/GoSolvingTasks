cd8b8459ce49188fe7b1442ac0743ef2e5426a85
Comprehensher <boltankove@gmail.com>
2024-07-30 15:34:35 +0300

    Preparation for chapter "Context"

--- coordination/main.go
+++ coordination/main.go
@@ -1,61 +1,25 @@
 package main
 
 import (
-	"math"
-	"math/rand"
 	"sync"
 	"time"
 )
 
-var waitGroup = sync.WaitGroup{}
-
-// var rwmutex = sync.RWMutex{}
-// var readyCond = sync.NewCond(rwmutex.RLocker())
-var once = sync.Once{}
-
-// var readyCond = sync.NewCond(&rwmutex)
-
-var squares = map[int]int{}
-
-func generateSquares(max int) {
-	// rwmutex.Lock()
-	Printfln("Generating data...")
-	for val := 0; val < max; val++ {
-		squares[val] = int(math.Pow(float64(val), 2))
+func processRequest(wg *sync.WaitGroup, count int) {
+	total := 0
+	for i := 0; i < count; i++ {
+		Printfln("Processing request: %v", total)
+		total++
+		time.Sleep(time.Millisecond * 250)
 	}
-	// rwmutex.Unlock()
-	// Printfln("Broadcasting condition")
-	// readyCond.Broadcast()
-	// waitGroup.Done()
-}
-
-func readSquares(id, max, iterations int) {
-	once.Do(func() {
-		generateSquares(max)
-	})
-	// readyCond.L.Lock()
-	// for len(squares) == 0 {
-	// 	readyCond.Wait()
-	// }
-	for i := 0; i < iterations; i++ {
-		key := rand.Intn(max)
-		Printfln("#%v Read value: %v = %v", id, key, squares[key])
-		time.Sleep(time.Millisecond * 100)
-	}
-	// readyCond.L.Unlock()
-	waitGroup.Done()
+	Printfln("Request processed...%v", total)
+	wg.Done()
 }
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	numRoutines := 2
-	waitGroup.Add(numRoutines)
-	for i := 0; i < numRoutines; i++ {
-		go readSquares(i, 10, 5)
-	}
-	time.Sleep(time.Millisecond * 200)
-	// waitGroup.Add(1)
-	// go generateSquares(10)
+	waitGroup := sync.WaitGroup{}
+	waitGroup.Add(1)
+	Printfln("Request dispatched...")
+	go processRequest(&waitGroup, 10)
 	waitGroup.Wait()
-	Printfln("Cached values: %v", len(squares))
 }
