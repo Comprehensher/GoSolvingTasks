b85c5a069dfc03e5ae2906c3b47c499e4c90d97f
Comprehensher <boltankove@gmail.com>
2024-07-30 14:53:33 +0300

    Coordinate goroutines. Another approach - sync.Once Method Do(func) executes generateSquares function only once before reading

--- coordination/main.go
+++ coordination/main.go
@@ -8,41 +8,41 @@ import (
 )
 
 var waitGroup = sync.WaitGroup{}
-var rwmutex = sync.RWMutex{}
 
-var readyCond = sync.NewCond(rwmutex.RLocker())
+// var rwmutex = sync.RWMutex{}
+// var readyCond = sync.NewCond(rwmutex.RLocker())
+var once = sync.Once{}
 
 // var readyCond = sync.NewCond(&rwmutex)
 
 var squares = map[int]int{}
 
 func generateSquares(max int) {
-	rwmutex.Lock()
+	// rwmutex.Lock()
 	Printfln("Generating data...")
 	for val := 0; val < max; val++ {
 		squares[val] = int(math.Pow(float64(val), 2))
 	}
-	rwmutex.Unlock()
-	Printfln("Broadcasting condition")
-	readyCond.Broadcast()
-	waitGroup.Done()
+	// rwmutex.Unlock()
+	// Printfln("Broadcasting condition")
+	// readyCond.Broadcast()
+	// waitGroup.Done()
 }
 
 func readSquares(id, max, iterations int) {
-	readyCond.L.Lock()
-	Printfln("Goroutine %v has got lock", id)
-	for len(squares) == 0 {
-		Printfln("Goroutine %v starts to wait...", id)
-		readyCond.Wait()
-		Printfln("Goroutine %v finish to wait...", id)
-	}
+	once.Do(func() {
+		generateSquares(max)
+	})
+	// readyCond.L.Lock()
+	// for len(squares) == 0 {
+	// 	readyCond.Wait()
+	// }
 	for i := 0; i < iterations; i++ {
 		key := rand.Intn(max)
 		Printfln("#%v Read value: %v = %v", id, key, squares[key])
 		time.Sleep(time.Millisecond * 100)
 	}
-	readyCond.L.Unlock()
-	Printfln("Goroutine %v released lock", id)
+	// readyCond.L.Unlock()
 	waitGroup.Done()
 }
 
@@ -54,8 +54,8 @@ func main() {
 		go readSquares(i, 10, 5)
 	}
 	time.Sleep(time.Millisecond * 200)
-	waitGroup.Add(1)
-	go generateSquares(10)
+	// waitGroup.Add(1)
+	// go generateSquares(10)
 	waitGroup.Wait()
 	Printfln("Cached values: %v", len(squares))
 }
