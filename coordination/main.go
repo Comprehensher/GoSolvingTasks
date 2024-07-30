5eb4f3976eae97991c80b292090ff9c54595e954
Comprehensher <boltankove@gmail.com>
2024-07-30 10:32:21 +0300

    Use Mutex. When several goroutines increment common variable

--- coordination/main.go
+++ coordination/main.go
@@ -2,22 +2,32 @@ package main
 
 import (
 	"sync"
+	"time"
 )
 
-func doSum(count int, val *int, waitGroup *sync.WaitGroup) {
+var waitGroup = sync.WaitGroup{}
+var mutex = sync.Mutex{}
+
+func doSum(count int, val *int) {
+	time.Sleep(time.Second * 2)
+	mutex.Lock()
 	for i := 0; i < count; i++ {
+
 		*val++
+
 	}
+	mutex.Unlock()
 	waitGroup.Done()
 }
 
 func main() {
 	counter := 0
 
-	waitGroup := sync.WaitGroup{}
-
-	waitGroup.Add(1)
-	doSum(5000, &counter, &waitGroup)
+	numRoutines := 3
+	waitGroup.Add(numRoutines)
+	for i := 0; i < numRoutines; i++ {
+		go doSum(5000, &counter)
+	}
 	waitGroup.Wait()
 	Printfln("Total: %v", counter)
 }
