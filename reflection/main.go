71e2b5318a874a7d606a79fc5b8260bdc906befe
Comprehensher <boltankove@gmail.com>
2024-07-31 12:28:36 +0300

    Get info about unknown passed type: Type name, Path (package where it is from) and Kind

--- reflection/main.go
+++ reflection/main.go
@@ -1,27 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"reflect"
-	"strings"
 )
+
+func getTypePath(t reflect.Type) (path string) {
+	path = t.PkgPath()
+	if path == "" {
+		path = "(built-in)"
+	}
+	return
+}
 
 func printDetails(values ...interface{}) {
 	for _, elem := range values {
-		fieldDetails := []string{}
 		elemType := reflect.TypeOf(elem)
-		elemValue := reflect.ValueOf(elem)
-		if elemType.Kind() == reflect.Struct {
-			for i := 0; i < elemType.NumField(); i++ {
-				fieldName := elemType.Field(i).Name
-				fieldVal := elemValue.Field(i)
-				fieldDetails = append(fieldDetails, fmt.Sprintf("%v: %v", fieldName, fieldVal))
-			}
-			Printfln("%v: %v", elemType.Name(),
-				strings.Join(fieldDetails, ", "))
-		} else {
-			Printfln("%v: %v", elemType.Name(), elemValue)
-		}
+		Printfln("Name: %v, PkgPath: %v, Kind: %v", elemType.Name(), getTypePath(elemType), elemType.Kind())
 	}
 }
 
