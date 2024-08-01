dac4c3a241071f0af28ad1e2aacdaf025652e5fa
Comprehensher <boltankove@gmail.com>
2024-08-01 14:17:29 +0300

    Pass slice and array in reflect function, and print all values and indexes of array/slice in that function

--- reflection/main.go
+++ reflection/main.go
@@ -4,28 +4,15 @@ import (
 	"reflect"
 )
 
-var stringPtrType = reflect.TypeOf((*string)(nil))
-
-func checkElemType(val interface{}, arrOrSlice interface{}) bool {
-	elemType := reflect.TypeOf(val)
-	arrOrSliceType := reflect.TypeOf(arrOrSlice)
-	return (arrOrSliceType.Kind() == reflect.Array ||
-		arrOrSliceType.Kind() == reflect.Slice) &&
-		arrOrSliceType.Elem() == elemType
-}
-
-func setValue(arrayOrSlice interface{}, index int, replacement interface{}) {
+func enumerateStrings(arrayOrSlice interface{}) {
 	arrayOrSliceVal := reflect.ValueOf(arrayOrSlice)
-	replacementVal := reflect.ValueOf(replacement)
-	if arrayOrSliceVal.Kind() == reflect.Slice {
-		elemVal := arrayOrSliceVal.Index(index)
-		if elemVal.CanSet() {
-			elemVal.Set(replacementVal)
+	if (arrayOrSliceVal.Kind() == reflect.Array ||
+		arrayOrSliceVal.Kind() == reflect.Slice) &&
+		arrayOrSliceVal.Type().Elem().Kind() == reflect.String {
+
+		for i := 0; i < arrayOrSliceVal.Len(); i++ {
+			Printfln("Element: %v, Value: %v", i, arrayOrSliceVal.Index(i).String())
 		}
-	} else if arrayOrSliceVal.Kind() == reflect.Ptr &&
-		arrayOrSliceVal.Elem().Kind() == reflect.Array &&
-		arrayOrSliceVal.Elem().CanSet() {
-		arrayOrSliceVal.Elem().Index(index).Set(replacementVal)
 	}
 }
 
@@ -35,12 +22,7 @@ func main() {
 	hobby := "Running"
 	slice := []string{name, city, hobby}
 	array := [3]string{name, city, hobby}
-	Printfln("Original slice: %v", slice)
-	newCity := "Paris"
-	setValue(slice, 1, newCity)
-	Printfln("Modified slice: %v", slice)
-	Printfln("Original array: %v", array)
-	newCity = "Rome"
-	setValue(&array, 1, newCity)
-	Printfln("Modified array: %v", array)
+
+	enumerateStrings(slice)
+	enumerateStrings(array)
 }
