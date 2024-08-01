d8d5fb2e070e1187d6ac46d0d4ae11b3cd29a7ce
Comprehensher <boltankove@gmail.com>
2024-08-01 16:27:34 +0300

    Get info about all fields of passed struct: index, name, Type, Exported, but info about nested struct should include also Use structType.FieldByIndex

--- reflection/main.go
+++ reflection/main.go
@@ -8,17 +8,22 @@ func inspectStructs(structs ...interface{}) {
 	for _, s := range structs {
 		structType := reflect.TypeOf(s)
 		if structType.Kind() == reflect.Struct {
-			inspectStructType(structType)
+			inspectStructType([]int{}, structType)
 		}
 	}
 }
 
-func inspectStructType(structType reflect.Type) {
+func inspectStructType(baseIndex []int, structType reflect.Type) {
 	Printfln("--- Struct Type: %v", structType)
 	for i := 0; i < structType.NumField(); i++ {
+		fieldIndex := append(baseIndex, i)
 		field := structType.Field(i)
 		Printfln("Field %v: Name: %v, Type: %v, Exported: %v",
-			field.Index, field.Name, field.Type, field.PkgPath == "")
+			fieldIndex, field.Name, field.Type, field.PkgPath == "")
+		if field.Type.Kind() == reflect.Struct {
+			field := structType.FieldByIndex(fieldIndex)
+			inspectStructType(fieldIndex, field.Type)
+		}
 	}
 	Printfln("--- End Struct Type: %v", structType)
 }
