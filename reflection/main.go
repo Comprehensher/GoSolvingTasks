5fae88a10318b91a19b539a1a313bccc84c60a60
Comprehensher <boltankove@gmail.com>
2024-08-01 17:08:20 +0300

    Pass struct with fields with tag into reflection function Get tags for struct's fields Print key and value of tags Use tag.Get() and teg.Lookup() methods

--- reflection/main.go
+++ reflection/main.go
@@ -4,25 +4,25 @@ import (
 	"reflect"
 )
 
-func describeField(s interface{}, fieldName string) {
+func inspectTags(s interface{}, tagName string) {
 	structType := reflect.TypeOf(s)
-	field, found := structType.FieldByName(fieldName)
-	if found {
-		Printfln("Found: %v, Type: %v, Index: %v",
-			field.Name, field.Type, field.Index)
-		index := field.Index
-		for len(index) > 1 {
-			index = index[0 : len(index)-1]
-			field = structType.FieldByIndex(index)
-			Printfln("Parent : %v, Type: %v, Index: %v",
-				field.Name, field.Type, field.Index)
-		}
-		Printfln("Top-Level Type: %v", structType)
-	} else {
-		Printfln("Field %v not found", fieldName)
+	for i := 0; i < structType.NumField(); i++ {
+		field := structType.Field(i)
+		tag := field.Tag
+		valGet := tag.Get(tagName)
+		valLookup, ok := tag.Lookup(tagName)
+		Printfln("Field: %v, Tag %v: %v", field.Name, tagName, valGet)
+		Printfln("Field: %v, Tag %v: %v, Set: %v",
+			field.Name, tagName, valLookup, ok)
 	}
 }
 
+type Person struct {
+	Name    string `alias:"id"`
+	City    string `alias:""`
+	Country string
+}
+
 func main() {
-	describeField(Purchase{}, "Price")
+	inspectTags(Person{}, "alias")
 }
