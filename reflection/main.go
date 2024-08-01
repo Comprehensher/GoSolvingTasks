92983df184235fd7df9c88d6d43a727e60357b94
Comprehensher <boltankove@gmail.com>
2024-08-01 12:23:09 +0300

    Change value in array and slice, by passing index and replacement value. Reflection function should work for both array and slice. Pay attention, it need to pass pointer to array

--- reflection/main.go
+++ reflection/main.go
@@ -14,13 +14,33 @@ func checkElemType(val interface{}, arrOrSlice interface{}) bool {
 		arrOrSliceType.Elem() == elemType
 }
 
+func setValue(arrayOrSlice interface{}, index int, replacement interface{}) {
+	arrayOrSliceVal := reflect.ValueOf(arrayOrSlice)
+	replacementVal := reflect.ValueOf(replacement)
+	if arrayOrSliceVal.Kind() == reflect.Slice {
+		elemVal := arrayOrSliceVal.Index(index)
+		if elemVal.CanSet() {
+			elemVal.Set(replacementVal)
+		}
+	} else if arrayOrSliceVal.Kind() == reflect.Ptr &&
+		arrayOrSliceVal.Elem().Kind() == reflect.Array &&
+		arrayOrSliceVal.Elem().CanSet() {
+		arrayOrSliceVal.Elem().Index(index).Set(replacementVal)
+	}
+}
+
 func main() {
 	name := "Alice"
 	city := "London"
 	hobby := "Running"
 	slice := []string{name, city, hobby}
 	array := [3]string{name, city, hobby}
-	Printfln("Slice (string): %v", checkElemType("testString", slice))
-	Printfln("Array (string): %v", checkElemType("testString", array))
-	Printfln("Array (int): %v", checkElemType(10, array))
+	Printfln("Original slice: %v", slice)
+	newCity := "Paris"
+	setValue(slice, 1, newCity)
+	Printfln("Modified slice: %v", slice)
+	Printfln("Original array: %v", array)
+	newCity = "Rome"
+	setValue(&array, 1, newCity)
+	Printfln("Modified array: %v", array)
 }
