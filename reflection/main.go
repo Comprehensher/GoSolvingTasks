86931e0716cfd61dad7757c61ab5bed9736fd946
Comprehensher <boltankove@gmail.com>
2024-08-01 15:04:21 +0300

    Pass Map to reflection function Iterate key and value of that Map. Use MapKeys() for iteration, access to value MapIndex mapValue.MapRange() to take iterator. And use iter.Next() for iteration

--- reflection/main.go
+++ reflection/main.go
@@ -7,9 +7,9 @@ import (
 func printMapContents(m interface{}) {
 	mapValue := reflect.ValueOf(m)
 	if mapValue.Kind() == reflect.Map {
-		for _, keyVal := range mapValue.MapKeys() {
-			reflectedVal := mapValue.MapIndex(keyVal)
-			Printfln("Map Key: %v, Value: %v", keyVal, reflectedVal)
+		iter := mapValue.MapRange()
+		for iter.Next() {
+			Printfln("Map Key: %v, Value: %v", iter.Key(), iter.Value())
 		}
 	} else {
 		Printfln("Not a map")
