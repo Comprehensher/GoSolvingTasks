c06f3e3a163487bf4c0f3e9e72867f7b977153de
Comprehensher <boltankove@gmail.com>
2024-08-01 15:54:59 +0300

    Create new map based on passed slice. Where key is element of slice value is upper(element) of slice. Use reflect.MapOf for definition map type(key and value type) and reflect.MakeMap for creation

--- reflection/main.go
+++ reflection/main.go
@@ -2,36 +2,33 @@ package main
 
 import (
 	"reflect"
+	"strings"
 )
 
-func setMap(m interface{}, key interface{}, val interface{}) {
-	mapValue := reflect.ValueOf(m)
-	keyValue := reflect.ValueOf(key)
-	valValue := reflect.ValueOf(val)
-	if mapValue.Kind() == reflect.Map &&
-		mapValue.Type().Key() == keyValue.Type() &&
-		mapValue.Type().Elem() == valValue.Type() {
-		mapValue.SetMapIndex(keyValue, valValue)
-	} else {
-		Printfln("Not a map or mismatched types")
+func createMap(slice interface{}, op func(interface{}) interface{}) interface{} {
+	sliceVal := reflect.ValueOf(slice)
+	if sliceVal.Kind() == reflect.Slice {
+		mapType := reflect.MapOf(sliceVal.Type().Elem(), sliceVal.Type().Elem())
+		mapVal := reflect.MakeMap(mapType)
+		for i := 0; i < sliceVal.Len(); i++ {
+			elemVal := sliceVal.Index(i)
+			mapVal.SetMapIndex(elemVal, reflect.ValueOf(op(elemVal.Interface())))
+		}
+		return mapVal.Interface()
 	}
-}
-
-func removeFromMap(m interface{}, key interface{}) {
-	mapValue := reflect.ValueOf(m)
-	keyValue := reflect.ValueOf(key)
-	if mapValue.Kind() == reflect.Map &&
-		mapValue.Type().Key() == keyValue.Type() {
-		mapValue.SetMapIndex(keyValue, reflect.Value{})
-	}
+	return nil
 }
 
 func main() {
-	pricesMap := map[string]float64{"Kayak": 279, "Lifejacket": 48.95, "Soccer Ball": 19.50}
-	setMap(pricesMap, "Kayak", 100.00)
-	setMap(pricesMap, "Hat", 10.00)
-	removeFromMap(pricesMap, "Lifejacket")
-	for k, v := range pricesMap {
-		Printfln("Key: %v, Value: %v", k, v)
+	names := []string{"Alice", "Bob", "Charlie"}
+	reverse := func(val interface{}) interface{} {
+		if str, ok := val.(string); ok {
+			return strings.ToUpper(str)
+		}
+		return val
+	}
+	namesMap := createMap(names, reverse).(map[string]string)
+	for k, v := range namesMap {
+		Printfln("Key: %v, Value:%v", k, v)
 	}
 }
