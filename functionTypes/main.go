f64a0418ad0b9eba04dd260ff696b76eee87ac1c
Comprehensher <boltankove@gmail.com>
2024-08-10 13:14:37 +0300

    Pass function as literal to parameter to another function

--- functionTypes/main.go
+++ functionTypes/main.go
@@ -17,17 +17,6 @@ func printPrice(product string, price float64, calculator calcFunc) {
 		calculator(price))
 }
 
-func selectCalculator(price float64) calcFunc {
-	if price > 100 {
-		return func(price float64) float64 {
-			return price + (price * 0.2)
-		}
-	}
-	return func(price float64) float64 {
-		return price
-	}
-}
-
 func main() {
 	products := map[string]float64{
 		"Kayak":      275,
@@ -35,6 +24,8 @@ func main() {
 	}
 
 	for product, price := range products {
-		printPrice(product, price, selectCalculator(price))
+		printPrice(product, price, func(price float64) float64 {
+			return price + (price * 0.2)
+		})
 	}
 }
