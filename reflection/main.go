bbccfd8b29424b72da9384b989a01fc437f580f0
Comprehensher <boltankove@gmail.com>
2024-08-12 07:49:46 +0300

    Print interface methods, include not exported fields.

--- reflection/main.go
+++ reflection/main.go
@@ -12,9 +12,19 @@ func getUnderlying(item Wrapper, fieldName string) {
 	itemVal := reflect.ValueOf(item)
 	fieldVal := itemVal.FieldByName(fieldName)
 	Printfln("Field Type: %v", fieldVal.Type())
+	for i := 0; i < fieldVal.Type().NumMethod(); i++ {
+		method := fieldVal.Type().Method(i)
+		Printfln("Interface Method: %v, Exported: %v",
+			method.Name, method.PkgPath == "")
+	}
+	Printfln("--------")
 	if fieldVal.Kind() == reflect.Interface {
 		Printfln("Underlying Type: %v",
 			fieldVal.Elem().Type())
+		for i := 0; i < fieldVal.Elem().Type().NumMethod(); i++ {
+			method := fieldVal.Elem().Type().Method(i)
+			Printfln("Underlying Method: %v", method.Name)
+		}
 	}
 }
 
