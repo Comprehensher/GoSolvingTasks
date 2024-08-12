ee3df5a573371a4dce4a8e8de08593e2fca31c9b
Comprehensher <boltankove@gmail.com>
2024-08-12 07:54:55 +0300

    Send some channel whithout value, and check its direction in some function

--- reflection/main.go
+++ reflection/main.go
@@ -4,30 +4,15 @@ import (
 	"reflect"
 )
 
-type Wrapper struct {
-	NamedItem
-}
-
-func getUnderlying(item Wrapper, fieldName string) {
-	itemVal := reflect.ValueOf(item)
-	fieldVal := itemVal.FieldByName(fieldName)
-	Printfln("Field Type: %v", fieldVal.Type())
-	for i := 0; i < fieldVal.Type().NumMethod(); i++ {
-		method := fieldVal.Type().Method(i)
-		Printfln("Interface Method: %v, Exported: %v",
-			method.Name, method.PkgPath == "")
-	}
-	Printfln("--------")
-	if fieldVal.Kind() == reflect.Interface {
-		Printfln("Underlying Type: %v",
-			fieldVal.Elem().Type())
-		for i := 0; i < fieldVal.Elem().Type().NumMethod(); i++ {
-			method := fieldVal.Elem().Type().Method(i)
-			Printfln("Underlying Method: %v", method.Name)
-		}
+func inspectChannel(channel interface{}) {
+	channelType := reflect.TypeOf(channel)
+	if channelType.Kind() == reflect.Chan {
+		Printfln("Type %v, Direction: %v",
+			channelType.Elem(), channelType.ChanDir())
 	}
 }
 
 func main() {
-	getUnderlying(Wrapper{NamedItem: &Product{}}, "NamedItem")
+	var c chan<- string
+	inspectChannel(c)
 }
