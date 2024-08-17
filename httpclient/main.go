1932eb2e2bb5e3ee3c09227f9d9f2134014197aa
Comprehensher <boltankove@gmail.com>
2024-08-17 16:14:33 +0300

    Send request and get response. But check response status is 200. Use ReadlAll(response.Body) for reading response body

--- httpclient/main.go
+++ httpclient/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"net/http"
 	"os"
 	"time"
@@ -10,9 +11,14 @@ func main() {
 	go http.ListenAndServe(":5000", nil)
 	time.Sleep(time.Second)
 	response, err := http.Get("http://localhost:5000/html")
-	if err == nil {
-		response.Write(os.Stdout)
+	if err == nil && response.StatusCode == http.StatusOK {
+		data, err := io.ReadAll(response.Body)
+		if err == nil {
+			defer response.Body.Close()
+			os.Stdout.Write(data)
+		}
 	} else {
-		Printfln("Error: %v", err.Error())
+		Printfln("Error: %v, Status Code: %v", err.Error(),
+			response.StatusCode)
 	}
 }
