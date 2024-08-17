4da5231e20f720bd0dca8154ee6e641a6a22d15a
Comprehensher <boltankove@gmail.com>
2024-08-17 16:11:24 +0300

    Send simple Get request

--- httpclient/main.go
+++ httpclient/main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"net/http"
+	"os"
+	"time"
 )
 
 func main() {
-	Printfln("Starting HTTP Server")
-	http.ListenAndServe(":5000", nil)
+	go http.ListenAndServe(":5000", nil)
+	time.Sleep(time.Second)
+	response, err := http.Get("http://localhost:5000/html")
+	if err == nil {
+		response.Write(os.Stdout)
+	} else {
+		Printfln("Error: %v", err.Error())
+	}
 }
