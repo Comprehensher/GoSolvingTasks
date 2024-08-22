85c9b26018f61269d46ab4d6544073c5241b6501
Comprehensher <boltankove@gmail.com>
2024-08-22 09:03:31 +0300

    Indicate max capacity so as if it overheaded this value then create new based array for this slice

--- slicesMaps/main.go
+++ slicesMaps/main.go
@@ -5,9 +5,10 @@ import "fmt"
 func main() {
 	products := [4]string{"Kayak", "Lifejacket", "Paddle",
 		"Hat"}
-	someNames := products[1:3]
+	someNames := products[1:3:3]
 	allNames := products[:]
 	someNames = append(someNames, "Gloves")
+	//someNames = append(someNames, "Boots")
 	fmt.Println("someNames:", someNames)
 	fmt.Println("someNames len:", len(someNames), "cap:",
 		cap(someNames))
