3a2f88ef81a52718f65f7220404202c3c3a40c8a
Comprehensher <boltankove@gmail.com>
2024-08-23 07:55:14 +0300

    Copy elements from one slice to another (function Copy()) but this time point from each element you want to copy to target slice, and each element from source slice

--- slicesMaps/main.go
+++ slicesMaps/main.go
@@ -6,8 +6,8 @@ func main() {
 	products := [4]string{"Kayak", "Lifejacket", "Paddle",
 		"Hat"}
 	allNames := products[1:]
-	someNames := make([]string, 2)
-	copy(someNames, allNames)
+	someNames := []string{"Boots", "Canoe"}
+	copy(someNames[1:], allNames[2:3])
 	fmt.Println("someNames:", someNames)
 	fmt.Println("allNames", allNames)
 }
