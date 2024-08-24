ca2d99a70e5379107649ec3a91ad601b46c04f41
Comprehensher <boltankove@gmail.com>
2024-08-24 10:43:53 +0300

    Initialize and create maps

--- slicesMaps/main.go
+++ slicesMaps/main.go
@@ -5,8 +5,10 @@ import (
 )
 
 func main() {
-	p1 := []string{"Kayak", "Lifejacket", "Paddle", "Hat"}
-	arrayPtr := (*[3]string)(p1)
-	array := *arrayPtr
-	fmt.Println(array)
+	products := make(map[string]float64, 10)
+	products["Kayak"] = 279
+	products["Lifejacket"] = 48.95
+	fmt.Println("Map size:", len(products))
+	fmt.Println("Price:", products["Kayak"])
+	fmt.Println("Price:", products["Hat"])
 }
