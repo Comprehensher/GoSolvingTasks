41318168a73d96c8def6a59db8dbd9a51425e24e
Comprehensher <boltankove@gmail.com>
2024-08-24 18:31:23 +0300

    Delete elements from map

--- slicesMaps/main.go
+++ slicesMaps/main.go
@@ -8,7 +8,9 @@ func main() {
 	products := map[string]float64{
 		"Kayak":      279,
 		"Lifejacket": 48.95,
+		"Hat":        0,
 	}
+	delete(products, "Hat")
 	if value, ok := products["Hat"]; ok {
 		fmt.Println("Stored value:", value)
 	} else {
