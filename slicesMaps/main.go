2858b20cb607d462620888473d00b96a33a92554
Comprehensher <boltankove@gmail.com>
2024-08-24 18:34:00 +0300

    Iterate over map

--- slicesMaps/main.go
+++ slicesMaps/main.go
@@ -10,10 +10,8 @@ func main() {
 		"Lifejacket": 48.95,
 		"Hat":        0,
 	}
-	delete(products, "Hat")
-	if value, ok := products["Hat"]; ok {
-		fmt.Println("Stored value:", value)
-	} else {
-		fmt.Println("No stored value")
+
+	for key, value := range products {
+		fmt.Println("Key:", key, "Value:", value)
 	}
 }
