04bbaeae010b845e18f15d0967019c701a65c16e
Comprehensher <boltankove@gmail.com>
2024-08-24 23:35:38 +0300

    Use function toTitle for capitalize every first symbol

--- stringsandregexp/main.go
+++ stringsandregexp/main.go
@@ -6,17 +6,7 @@ import (
 )
 
 func main() {
-	product := "Kayak"
-	fmt.Println("Contains:", strings.Contains(product,
-		"yak"))
-	fmt.Println("ContainsAny:", strings.ContainsAny(product,
-		"abc"))
-	fmt.Println("ContainsRune:",
-		strings.ContainsRune(product, 'K'))
-	fmt.Println("EqualFold:", strings.EqualFold(product,
-		"KAYAK"))
-	fmt.Println("HasPrefix:", strings.HasPrefix(product,
-		"Ka"))
-	fmt.Println("HasSuffix:", strings.HasSuffix(product,
-		"yak"))
+	description := "A boat for sailing"
+	fmt.Println("Original:", description)
+	fmt.Println("Title:", strings.Title(description))
 }
