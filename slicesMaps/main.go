7a03b31c1e2f9c6f52c7a4915f8f6958b0e5d4d5
Comprehensher <boltankove@gmail.com>
2024-08-24 10:50:52 +0300

    Initialize map with predefined values

--- slicesMaps/main.go
+++ slicesMaps/main.go
@@ -5,9 +5,10 @@ import (
 )
 
 func main() {
-	products := make(map[string]float64, 10)
-	products["Kayak"] = 279
-	products["Lifejacket"] = 48.95
+	products := map[string]float64{
+		"Kayak":      279,
+		"Lifejacket": 48.95,
+	}
 	fmt.Println("Map size:", len(products))
 	fmt.Println("Price:", products["Kayak"])
 	fmt.Println("Price:", products["Hat"])
