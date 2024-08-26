abdf90fc183ad2f01cec6244320934dd569ce77a
Comprehensher <boltankove@gmail.com>
2024-08-26 11:20:01 +0300

    Get index of definite letter with using user function This function should check rune for that definite symbol

--- stringsandregexp/main.go
+++ stringsandregexp/main.go
@@ -7,14 +7,9 @@ import (
 
 func main() {
 	description := "A boat for one person"
-	fmt.Println("Count:", strings.Count(description, "o"))
-	fmt.Println("Index:", strings.Index(description, "o"))
-	fmt.Println("LastIndex:", strings.LastIndex(description,
-		"o"))
-	fmt.Println("IndexAny:", strings.IndexAny(description,
-		"abcd"))
-	fmt.Println("LastIndex:", strings.LastIndex(description,
-		"o"))
-	fmt.Println("LastIndexAny:",
-		strings.LastIndexAny(description, "abcd"))
+	isLetterB := func(r rune) bool {
+		return r == 'B' || r == 'b'
+	}
+	fmt.Println("IndexFunc:", strings.IndexFunc(description,
+		isLetterB))
 }
