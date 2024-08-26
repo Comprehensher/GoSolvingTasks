f755c7561924a7b43b3f42572d23fb21f204cf31
Comprehensher <boltankove@gmail.com>
2024-08-26 16:03:25 +0300

    Create row with using strings.Builder Append additional words by using builder.WriteString Print results by using builder.String()

--- stringsandregexp/main.go
+++ stringsandregexp/main.go
@@ -7,7 +7,14 @@ import (
 
 func main() {
 	text := "It was a boat. A small boat."
-	elements := strings.Fields(text)
-	joined := strings.Join(elements, "--")
-	fmt.Println("Joined:", joined)
+
+	var builder strings.Builder
+	for _, sub := range strings.Fields(text) {
+		if sub == "small" {
+			builder.WriteString("very ")
+		}
+		builder.WriteString(sub)
+		builder.WriteRune(' ')
+	}
+	fmt.Println("String:", builder.String())
 }
