347832afec2f6bc325199fb01f98b6dd97075f19
Comprehensher <boltankove@gmail.com>
2024-08-26 11:09:07 +0300

    Use strings.Count() in order to count how much definite symbol in row strings.Index() - print what index the symbol has in row strings.LastIndex - last index in row for definite symbol strings.IndexAny - first entry of symbol to row

--- stringsandregexp/main.go
+++ stringsandregexp/main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"fmt"
-	"unicode"
+	"strings"
 )
 
 func main() {
-	product := "Kayak"
-	for _, char := range product {
-		fmt.Println(string(char), "Upper case:",
-			unicode.IsUpper(char))
-	}
+	description := "A boat for one person"
+	fmt.Println("Count:", strings.Count(description, "o"))
+	fmt.Println("Index:", strings.Index(description, "o"))
+	fmt.Println("LastIndex:", strings.LastIndex(description,
+		"o"))
+	fmt.Println("IndexAny:", strings.IndexAny(description,
+		"abcd"))
+	fmt.Println("LastIndex:", strings.LastIndex(description,
+		"o"))
+	fmt.Println("LastIndexAny:",
+		strings.LastIndexAny(description, "abcd"))
 }
