24ae753b686ee92bf9d633371473d51e624a94e6
Comprehensher <boltankove@gmail.com>
2024-08-27 08:14:49 +0300

    Print names and values of fields of some struct

--- usingstrings/main.go
+++ usingstrings/main.go
@@ -5,9 +5,7 @@ import "fmt"
 func Printfln(template string, values ...interface{}) {
 	fmt.Printf(template+"\n", values...)
 }
-
 func main() {
 	Printfln("Value: %v", Kayak)
-	Printfln("Go syntax: %#v", Kayak)
-	Printfln("Type: %T", Kayak)
+	Printfln("Value with fields: %+v", Kayak)
 }
