566390e6797e4c935e7dbe177861b480bbd2fdf7
Comprehensher <boltankove@gmail.com>
2024-08-27 08:08:45 +0300

    Print value of fields of some struct, print struct with values, print type of struct, use Printf()

--- usingstrings/main.go
+++ usingstrings/main.go
@@ -2,18 +2,12 @@ package main
 
 import "fmt"
 
-func getProductName(index int) (name string, err error) {
-	if len(Products) > index {
-		name = fmt.Sprintf("Name of product: %v",
-			Products[index].Name)
-	} else {
-		err = fmt.Errorf("Error for index %v", index)
-	}
-	return
+func Printfln(template string, values ...interface{}) {
+	fmt.Printf(template+"\n", values...)
 }
+
 func main() {
-	name, _ := getProductName(1)
-	fmt.Println(name)
-	_, err := getProductName(10)
-	fmt.Println(err.Error())
+	Printfln("Value: %v", Kayak)
+	Printfln("Go syntax: %#v", Kayak)
+	Printfln("Type: %T", Kayak)
 }
