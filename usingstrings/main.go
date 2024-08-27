0ed90d8930e40091c94e75092ea8562e0001d732
Comprehensher <boltankove@gmail.com>
2024-08-27 08:25:46 +0300

    Use some integer variable, print it as integer, as binary, as 8-bit, as 16-bit string

--- usingstrings/main.go
+++ usingstrings/main.go
@@ -6,6 +6,9 @@ func Printfln(template string, values ...interface{}) {
 	fmt.Printf(template+"\n", values...)
 }
 func main() {
-	Printfln("Value: %v", Kayak)
-	Printfln("Value with fields: %+v", Kayak)
+	number := 250
+	Printfln("Binary: %b", number)
+	Printfln("Decimal: %d", number)
+	Printfln("Octal: %o, %O", number, number)
+	Printfln("Hexadecimal: %x, %X", number, number)
 }
