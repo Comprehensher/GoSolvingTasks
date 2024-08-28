891ab8b14fc6b46a1598a37af3472de68bb2051b
Comprehensher <boltankove@gmail.com>
2024-08-28 07:36:45 +0300

    Allow user to input three values Scan (use fmt.Scan()) these values and print them

--- usingstrings/main.go
+++ usingstrings/main.go
@@ -6,6 +6,16 @@ func Printfln(template string, values ...interface{}) {
 	fmt.Printf(template+"\n", values...)
 }
 func main() {
-	name := "Kayak"
-	Printfln("Pointer: %p", &name)
+	var name string
+	var category string
+	var price float64
+	fmt.Print("Enter text to scan: ")
+	n, err := fmt.Scan(&name, &category, &price)
+	if err == nil {
+		Printfln("Scanned %v values", n)
+		Printfln("Name: %v, Category: %v, Price: %.2f", name,
+			category, price)
+	} else {
+		Printfln("Error: %v", err.Error())
+	}
 }
