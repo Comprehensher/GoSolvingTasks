e1dc4cc82dabd97bc5282b8a48eb89b1199f1ed3
Comprehensher <boltankove@gmail.com>
2024-08-28 07:20:22 +0300

    Formatting string e and point

--- usingstrings/main.go
+++ usingstrings/main.go
@@ -6,9 +6,9 @@ func Printfln(template string, values ...interface{}) {
 	fmt.Printf(template+"\n", values...)
 }
 func main() {
-	number := 250
-	Printfln("Binary: %b", number)
-	Printfln("Decimal: %d", number)
-	Printfln("Octal: %o, %O", number, number)
+	number := 279.00
+	Printfln("Decimalless with exponent: %b", number)
+	Printfln("Decimal with exponent: %e", number)
+	Printfln("Decimal without exponent: %f", number)
 	Printfln("Hexadecimal: %x, %X", number, number)
 }
