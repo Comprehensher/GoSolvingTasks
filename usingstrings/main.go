c80b2df99179405b98fe7d528730fb5b8b89efc1
Comprehensher <boltankove@gmail.com>
2024-08-28 07:23:50 +0300

    Print some rune and byte of first symbol in string

--- usingstrings/main.go
+++ usingstrings/main.go
@@ -6,9 +6,8 @@ func Printfln(template string, values ...interface{}) {
 	fmt.Printf(template+"\n", values...)
 }
 func main() {
-	number := 279.00
-	Printfln("Decimalless with exponent: %b", number)
-	Printfln("Decimal with exponent: %e", number)
-	Printfln("Decimal without exponent: %f", number)
-	Printfln("Hexadecimal: %x, %X", number, number)
+	name := "Kayak"
+	Printfln("String: %s", name)
+	Printfln("Character: %c", []rune(name)[0])
+	Printfln("Unicode: %U", []rune(name)[0])
 }
