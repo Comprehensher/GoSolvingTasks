6bd278a5dd9730ff8b71c47eede151b35efd70d4
Comprehensher <boltankove@gmail.com>
2024-08-29 09:21:30 +0300

    Sort user custom types. For i.e. for struct Product, sort for field Price. So, create slice of products and sort them with usage Interface and implemnet its methods: Len(), Less(), Swap()

--- sorting/main.go
+++ sorting/main.go
@@ -1,22 +1,16 @@
 package main
 
-import (
-	//"math/rand"
-	//"time"
-	"sort"
-)
+//"math/rand"
+//"time"
 
 func main() {
-	ints := []int{9, 4, 2, -1, 10}
-	sortedInts := make([]int, len(ints))
-	copy(sortedInts, ints)
-	sort.Ints(sortedInts)
-	Printfln("Ints: %v", ints)
-	Printfln("Ints Sorted: %v", sortedInts)
-	indexOf4 := sort.SearchInts(sortedInts, 4)
-	indexOf3 := sort.SearchInts(sortedInts, 3)
-	Printfln("Index of 4: %v (present: %v)", indexOf4,
-		sortedInts[indexOf4] == 4)
-	Printfln("Index of 3: %v (present: %v)", indexOf3,
-		sortedInts[indexOf3] == 3)
+	products := []Product{
+		{"Kayak", 279},
+		{"Lifejacket", 49.95},
+		{"Soccer Ball", 19.50},
+	}
+	ProductSlices(products)
+	for _, p := range products {
+		Printfln("Name: %v, Price: %.2f", p.Name, p.Price)
+	}
 }
