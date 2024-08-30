6cc7299c0cf567dfd96d7ace9bbefc43d1752ffa
Comprehensher <boltankove@gmail.com>
2024-08-30 21:17:16 +0300

    Write how many years remain to some future year Write how many years passed since past year

--- dateandtime/main.go
+++ dateandtime/main.go
@@ -6,15 +6,11 @@ import (
 )
 
 func main() {
-	var d time.Duration = time.Hour + (30 * time.Minute)
-	Printfln("Hours: %v", d.Hours())
-	Printfln("Mins: %v", d.Minutes())
-	Printfln("Seconds: %v", d.Seconds())
-	Printfln("Millseconds: %v", d.Milliseconds())
-	rounded := d.Round(time.Hour)
-	Printfln("Rounded Hours: %v", rounded.Hours())
-	Printfln("Rounded Mins: %v", rounded.Minutes())
-	trunc := d.Truncate(time.Hour)
-	Printfln("Truncated Hours: %v", trunc.Hours())
-	Printfln("Rounded Mins: %v", trunc.Minutes())
+	toYears := func(d time.Duration) int {
+		return int(d.Hours() / (24 * 365))
+	}
+	future := time.Date(2051, 0, 0, 0, 0, 0, 0, time.Local)
+	past := time.Date(1965, 0, 0, 0, 0, 0, 0, time.Local)
+	Printfln("Future: %v", toYears(time.Until(future)))
+	Printfln("Past: %v", toYears(time.Since(past)))
 }
