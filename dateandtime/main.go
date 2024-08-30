1fce0cf0d1d1209d6380419c4715952d3f0e3091
Comprehensher <boltankove@gmail.com>
2024-08-30 08:19:01 +0300

    Convert string to date (Use time.Parse())

---
+++ dateandtime/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func PrintTime(label string, t *time.Time) {
+	//layout := "Day: 02 Month: Jan Year: 2006"
+	fmt.Println(label, t.Format(time.RFC822Z))
+}
+func main() {
+	layout := "2006-Jan-02"
+	dates := []string{
+		"1995-Jun-09",
+		"2015-Jun-02",
+	}
+	for _, d := range dates {
+		time, err := time.Parse(layout, d)
+		if err == nil {
+			PrintTime("Parsed", &time)
+		} else {
+			Printfln("Error: %s", err.Error())
+		}
+	}
+}
