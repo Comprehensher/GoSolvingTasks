5e2b40e94980df24fe3e5c3a0ae69bedb40f44ee
Comprehensher <boltankove@gmail.com>
2024-08-30 21:24:24 +0300

    Get Duration from some string, use time.ParseDuration()

--- dateandtime/main.go
+++ dateandtime/main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	// "fmt"
+	"fmt"
 	"time"
 )
 
 func main() {
-	toYears := func(d time.Duration) int {
-		return int(d.Hours() / (24 * 365))
+	d, err := time.ParseDuration("1h30m")
+	if err == nil {
+		Printfln("Hours: %v", d.Hours())
+		Printfln("Mins: %v", d.Minutes())
+		Printfln("Seconds: %v", d.Seconds())
+		Printfln("Millseconds: %v", d.Milliseconds())
+	} else {
+		fmt.Println(err.Error())
 	}
-	future := time.Date(2051, 0, 0, 0, 0, 0, 0, time.Local)
-	past := time.Date(1965, 0, 0, 0, 0, 0, 0, time.Local)
-	Printfln("Future: %v", toYears(time.Until(future)))
-	Printfln("Past: %v", toYears(time.Since(past)))
 }
