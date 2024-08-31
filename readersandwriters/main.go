d4f6db906835c05eeb1234957d3d43b8e6667cc9
Comprehensher <boltankove@gmail.com>
2024-08-31 21:47:49 +0300

    Read from simple string with io.Reader Print this string from read value

--- readersandwriters/main.go
+++ readersandwriters/main.go
@@ -1,6 +1,24 @@
 package main
 
+import (
+	"io"
+	"strings"
+)
+
+func processData(reader io.Reader) {
+	b := make([]byte, 2)
+	for {
+		count, err := reader.Read(b)
+		if count > 0 {
+			Printfln("Read %v bytes: %v", count,
+				string(b[0:count]))
+		}
+		if err == io.EOF {
+			break
+		}
+	}
+}
 func main() {
-	Printfln("Product: %v, Price : %v", Kayak.Name,
-		Kayak.Price)
+	r := strings.NewReader("Kayak")
+	processData(r)
 }
