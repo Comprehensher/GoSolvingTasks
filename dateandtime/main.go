9cefb7b97dcfe719663479bcbba41cb7e9f4bd4f
Comprehensher <boltankove@gmail.com>
2024-08-31 17:49:41 +0300

    Create timer with 10 min for delaying in goroutine But that goroutine should reset timer to 1 sec. Use timer.Reset()

--- dateandtime/main.go
+++ dateandtime/main.go
@@ -6,32 +6,26 @@ import (
 )
 
 func writeToChannel(channel chan<- string) {
+	timer := time.NewTimer(time.Minute * 10)
+	go func() {
+		time.Sleep(time.Second * 2)
+		Printfln("Resetting timer")
+		timer.Reset(time.Second * 1)
+	}()
 	Printfln("Waiting for initial duration...")
-	_ = <-time.After(time.Second * 2)
+	<-timer.C
 	Printfln("Initial duration elapsed.")
 	names := []string{"Alice", "Bob", "Charlie", "Dora"}
 	for _, name := range names {
 		channel <- name
-		time.Sleep(time.Second * 3)
+		//time.Sleep(time.Second * 3)
 	}
 	close(channel)
 }
 func main() {
 	nameChannel := make(chan string)
 	go writeToChannel(nameChannel)
-	channelOpen := true
-	for channelOpen {
-		Printfln("Starting channel read")
-		select {
-		case name, ok := <-nameChannel:
-			if !ok {
-				channelOpen = false
-				break
-			} else {
-				Printfln("Read name: %v", name)
-			}
-		case <-time.After(time.Second * 2):
-			Printfln("Timeout")
-		}
+	for name := range nameChannel {
+		Printfln("Read name: %v", name)
 	}
 }
