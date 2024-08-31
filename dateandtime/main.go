872ac1ade4b02c99d20d20fc2f60e39241003cd0
Comprehensher <boltankove@gmail.com>
2024-08-31 17:24:20 +0300

    Create your own goroutine and delay it for 2 sec, use time.After()

--- dateandtime/main.go
+++ dateandtime/main.go
@@ -6,17 +6,19 @@ import (
 )
 
 func writeToChannel(channel chan<- string) {
+	Printfln("Waiting for initial duration...")
+	_ = <-time.After(time.Second * 2)
+	Printfln("Initial duration elapsed.")
 	names := []string{"Alice", "Bob", "Charlie", "Dora"}
 	for _, name := range names {
 		channel <- name
+		time.Sleep(time.Second * 1)
 	}
 	close(channel)
 }
 func main() {
 	nameChannel := make(chan string)
-	time.AfterFunc(time.Second*5, func() {
-		writeToChannel(nameChannel)
-	})
+	go writeToChannel(nameChannel)
 	for name := range nameChannel {
 		Printfln("Read name: %v", name)
 	}
