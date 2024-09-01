06cc95433269e25d01389f5a954c1e13a4d32fbb
Comprehensher <boltankove@gmail.com>
2024-09-01 17:50:26 +0300

    Encode map, and print out the results.

--- readersandwriters/main.go
+++ readersandwriters/main.go
@@ -7,16 +7,13 @@ import (
 )
 
 func main() {
-	names := []string{"Kayak", "Lifejacket", "Soccer Ball"}
-	numbers := [3]int{10, 20, 30}
-	var byteArray [5]byte
-	copy(byteArray[0:], []byte(names[0]))
-	byteSlice := []byte(names[0])
+	m := map[string]float64{
+		"Kayak":      279,
+		"Lifejacket": 49.95,
+	}
 	var writer strings.Builder
 	encoder := json.NewEncoder(&writer)
-	encoder.Encode(names)
-	encoder.Encode(numbers)
-	encoder.Encode(byteArray)
-	encoder.Encode(byteSlice)
+	encoder.Encode(m)
+
 	fmt.Print(writer.String())
 }
