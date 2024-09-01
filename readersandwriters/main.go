56796fe58a220303e1d79676886e400bf2a48f4d
Comprehensher <boltankove@gmail.com>
2024-09-01 15:53:06 +0300

    Scan string(use fmt.Fscanf()) from io.Reader and put results to three variables with using templating

--- readersandwriters/main.go
+++ readersandwriters/main.go
@@ -1,23 +1,28 @@
 package main
 
 import (
-	"bufio"
+	"fmt"
+	"io"
 	"strings"
 )
 
+func scanFromReader(reader io.Reader, template string,
+	vals ...interface{}) (int, error) {
+	return fmt.Fscanf(reader, template, vals...)
+}
+
 func main() {
-	text := "It was a boat. A small boat."
-	var builder strings.Builder
-	var writer = bufio.NewWriterSize(NewCustomWriter(&builder), 20)
-	for i := 0; true; {
-		end := i + 5
-		if end >= len(text) {
-			writer.Write([]byte(text[i:]))
-			writer.Flush()
-			break
-		}
-		writer.Write([]byte(text[i:end]))
-		i = end
+	reader := strings.NewReader("Kayak Watersports $279.00")
+	var name, category string
+	var price float64
+	scanTemplate := "%s %s $%f"
+	_, err := scanFromReader(reader, scanTemplate, &name,
+		&category, &price)
+	if err != nil {
+		Printfln("Error: %v", err.Error())
+	} else {
+		Printfln("Name: %v", name)
+		Printfln("Category: %v", category)
+		Printfln("Price: %.2f", price)
 	}
-	Printfln("Written data: %v", builder.String())
 }
