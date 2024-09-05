2ad8f8c30568e4587f4fad80b57043b969fd2693
Comprehensher <boltankove@gmail.com>
2024-09-05 09:11:20 +0300

    Same as in previous, but read file not to slice, but to File. Use os.Open("your_file_name")

--- files/readconfig.go
+++ files/readconfig.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"os"
-	"strings"
 )
 
 type ConfigData struct {
@@ -14,14 +13,15 @@ type ConfigData struct {
 var Config ConfigData
 
 func LoadConfig() (err error) {
-	data, err := os.ReadFile("config.json")
+	file, err := os.Open("config.json")
 	if err == nil {
-		decoder :=
-			json.NewDecoder(strings.NewReader(string(data)))
+		defer file.Close()
+		decoder := json.NewDecoder(file)
 		err = decoder.Decode(&Config)
 	}
 	return
 }
+
 func init() {
 	err := LoadConfig()
 	if err != nil {
