fa1a33d6d941de5c1ac4be74cb1ec0a94f55c289
Comprehensher <boltankove@gmail.com>
2024-09-06 08:52:26 +0300

    Create folder with file in your home directory

--- files/main.go
+++ files/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"os"
 	"path/filepath"
 )
@@ -12,8 +13,17 @@ func main() {
 			"MyTempFile.json")
 	}
 	Printfln("Full path: %v", path)
-	Printfln("Volume name: %v", filepath.VolumeName(path))
-	Printfln("Dir component: %v", filepath.Dir(path))
-	Printfln("File component: %v", filepath.Base(path))
-	Printfln("File extension: %v", filepath.Ext(path))
+	err = os.MkdirAll(filepath.Dir(path), 0766)
+	if err == nil {
+		file, err := os.OpenFile(path, os.O_CREATE|
+			os.O_WRONLY, 0666)
+		if err == nil {
+			defer file.Close()
+			encoder := json.NewEncoder(file)
+			encoder.Encode(Products)
+		}
+	}
+	if err != nil {
+		Printfln("Error %v", err.Error())
+	}
 }
