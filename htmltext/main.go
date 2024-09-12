f5e60eb8a3479a11ac898958420f49581f86a3f0
Comprehensher <boltankove@gmail.com>
2024-09-12 08:23:46 +0300

    Same as in previous, but execute two templates with using one varibale for all templates use ExecuteTemplate() in order to execute definite template

--- htmltext/main.go
+++ htmltext/main.go
@@ -6,14 +6,16 @@ import (
 )
 
 func main() {
-	t1, err1 :=
-		template.ParseFiles("templates/template.html")
-	t2, err2 := template.ParseFiles("templates/extras.html")
-	if err1 == nil && err2 == nil {
-		t1.Execute(os.Stdout, &Kayak)
-		os.Stdout.WriteString("\n")
-		t2.Execute(os.Stdout, &Kayak)
-	} else {
-		Printfln("Error: %v %v", err1.Error(), err2.Error())
-	}
+	allTemplates, err1 :=
+template.ParseFiles("templates/template.html",
+"templates/extras.html")
+if (err1 == nil) {
+allTemplates.ExecuteTemplate(os.Stdout,
+"template.html", &Kayak)
+os.Stdout.WriteString("\n")
+allTemplates.ExecuteTemplate(os.Stdout,
+"extras.html", &Kayak)
+} else {
+Printfln("Error: %v %v", err1.Error())
+}
 }
