6167fe00bd85834b8fc338c3414307404b0bd258
Comprehensher <boltankove@gmail.com>
2024-09-12 08:19:36 +0300

    Same as in previous, but execute two templates with using two different values

--- htmltext/main.go
+++ htmltext/main.go
@@ -6,10 +6,14 @@ import (
 )
 
 func main() {
-	t, err := template.ParseFiles("templates/template.html")
-	if err == nil {
-		t.Execute(os.Stdout, &Kayak)
+	t1, err1 :=
+		template.ParseFiles("templates/template.html")
+	t2, err2 := template.ParseFiles("templates/extras.html")
+	if err1 == nil && err2 == nil {
+		t1.Execute(os.Stdout, &Kayak)
+		os.Stdout.WriteString("\n")
+		t2.Execute(os.Stdout, &Kayak)
 	} else {
-		Printfln("Error: %v", err.Error())
+		Printfln("Error: %v %v", err1.Error(), err2.Error())
 	}
 }
