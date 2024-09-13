74465bfb82d6b72e56c93c98e5e1db8d1402dd04
Comprehensher <boltankove@gmail.com>
2024-09-13 08:36:21 +0300

    Load all templates with *.html extension in definite folder (Use template.ParseGlob) Show its names (use allTemplates.Templates() for getting slice and t.Name() for getting name)

--- htmltext/main.go
+++ htmltext/main.go
@@ -1,21 +1,17 @@
 package main
 
 import (
-	"os"
 	"text/template"
 )
 
 func main() {
-	allTemplates, err1 :=
-template.ParseFiles("templates/template.html",
-"templates/extras.html")
-if (err1 == nil) {
-allTemplates.ExecuteTemplate(os.Stdout,
-"template.html", &Kayak)
-os.Stdout.WriteString("\n")
-allTemplates.ExecuteTemplate(os.Stdout,
-"extras.html", &Kayak)
-} else {
-Printfln("Error: %v %v", err1.Error())
-}
+	allTemplates, err :=
+		template.ParseGlob("templates/*.html")
+	if err == nil {
+		for _, t := range allTemplates.Templates() {
+			Printfln("Template name: %v", t.Name())
+		}
+	} else {
+		Printfln("Error: %v %v", err.Error())
+	}
 }
