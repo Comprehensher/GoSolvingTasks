1927c6257ecdf5f2401ec985b234c3b7ce1c27cb
Comprehensher <boltankove@gmail.com>
2024-10-03 07:55:04 +0300

    Declare several variables without type with one operator. And assign them initial values. And example for not assign.

--- technics/main.go
+++ technics/main.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var price float32
-	fmt.Println(price)
-	price = 275.00
-	fmt.Println(price)
+	var price, tax = 275.00, 27.50
+	fmt.Println(price + tax)
+
+	var price1, tax1 float64
+	price1 = 275.00
+	tax1 = 27.50
+	fmt.Println(price1 + tax1)
 }
