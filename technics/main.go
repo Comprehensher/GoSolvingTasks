5dc977e8e6ac7246810a6a6d94485413f71bbc02
Comprehensher <boltankove@gmail.com>
2024-10-03 08:05:29 +0300

    Use empty identifier when you don’t need to use variable

--- technics/main.go
+++ technics/main.go
@@ -3,9 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	price, tax, inStock := 275.00, 27.50, true
+	price, tax, inStock, _ := 275.00, 27.50, true, true
+	var _ = "Alice"
 	fmt.Println("Total:", price+tax)
 	fmt.Println("In stock:", inStock)
-	price2, tax := 200.00, 25.00
-	fmt.Println("Total 2:", price2+tax)
 }
