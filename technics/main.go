a9358ce3c0ea3e297287641d2052c9fe71334b49
Comprehensher <boltankove@gmail.com>
2024-10-03 07:57:11 +0300

    Declare and assign variable with faster way, not using its type

--- technics/main.go
+++ technics/main.go
@@ -3,11 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var price, tax = 275.00, 27.50
-	fmt.Println(price + tax)
-
-	var price1, tax1 float64
-	price1 = 275.00
-	tax1 = 27.50
-	fmt.Println(price1 + tax1)
+	price, tax, inStock := 275.00, 27.50, true
+	fmt.Println("Total:", price+tax)
+	fmt.Println("In stock:", inStock)
 }
