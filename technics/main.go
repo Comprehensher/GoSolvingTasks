8ed41bf158add30f4d23a3669430927efac6889a
Comprehensher <boltankove@gmail.com>
2024-10-03 07:44:43 +0300

    Declare two variables: using var with and without type

--- technics/main.go
+++ technics/main.go
@@ -3,11 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	const price, tax float32 = 275, 27.50
-	fmt.Println("price:", price)
-	fmt.Println("tax:", tax)
-
-	const CONSUME, isTrue = 1, true
-	fmt.Println("CONSUME:", CONSUME)
-	fmt.Println("isTrue:", isTrue)
+	var price float64 = 275.00
+	var price2 = price
+	fmt.Println(price)
+	fmt.Println(price2)
 }
