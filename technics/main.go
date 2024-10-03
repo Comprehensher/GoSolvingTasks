0155fe2e9d7203d29c6372b4af7bc16965629523
Comprehensher <boltankove@gmail.com>
2024-10-03 08:02:37 +0300

    Override variable, (it can be possible if you don't use var)

--- technics/main.go
+++ technics/main.go
@@ -6,4 +6,6 @@ func main() {
 	price, tax, inStock := 275.00, 27.50, true
 	fmt.Println("Total:", price+tax)
 	fmt.Println("In stock:", inStock)
+	price2, tax := 200.00, 25.00
+	fmt.Println("Total 2:", price2+tax)
 }
